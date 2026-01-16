48aedab6f7577d1519a2077bc8923121f5d9e5c8
Nayane Oi <nayane_rms@hotmail.com>
2026-01-16 10:41:05 -0300

    Adicionando Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Cadastro from "./cadastro/Cadastro";
 
 function App() {
   return (
     <>
-    <BrowserRouter>    
-    <Navbar />
-    <div className="min-h-[80vh]">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </div>
-    <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Cadastro />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-  
+
   );
 }
 
