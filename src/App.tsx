b812383109f5fbbcd9fc8ad8abff91f9b8db1298
Nayane Oi <nayane_rms@hotmail.com>
2026-01-16 10:44:31 -0300

    Adicionando Login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Cadastro from "./cadastro/Cadastro";
+import Login from "./login/Login";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
+            <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/" element={<Cadastro />} />
+            <Route path="/cadastro" element={<Cadastro />} />
           </Routes>
         </div>
         <Footer />
