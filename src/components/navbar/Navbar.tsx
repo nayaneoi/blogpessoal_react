d4026cc8f0bc8e7baf87438d21c4552efd7052f0
Nayane Oi <nayane_rms@hotmail.com>
2026-01-16 12:55:06 -0300

    Adicionando Lógica do Login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+        handleLogout();
+        alert("O usuário foi deslogado com sucesso!");
+        navigate("/")
+    }
     return (
         <>
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
-            
+
                 <div className="container flex justify-between text-lg mx-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
@@ -14,7 +26,7 @@ function Navbar() {
                         Temas
                         Cadastrar tema
                         Perfil
-                        Sair
+                        <Link to='' onClick={logout} className="houver:underline">Sair</Link>
                     </div>
                 </div>
             </div>
